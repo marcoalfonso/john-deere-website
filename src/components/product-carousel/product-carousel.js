312d0d47415cab82b16e82f338f988a238e17a83
Marco Lavielle <marcoalfonso@gmail.com>
2019-03-18 17:42:11 +1100

    feature/product-carousel-responsive

--- src/components/product-carousel/product-carousel.js
+++ src/components/product-carousel/product-carousel.js
@@ -1,49 +1,59 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Link } from 'gatsby'
 import Img from 'gatsby-image'
 import Tabs from 'react-bootstrap/Tabs'
 import Tab from 'react-bootstrap/Tab'
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup } from 'pure-react-carousel';
-
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup } from 'pure-react-carousel'
+import windowSize from 'react-window-size'
 import styles from './product-carousel.module.css'
 
-export default ({ categories, products }) => {
-  console.log("categories", categories)
-  console.log("products", products.length)
-  return (
-    <section className={styles.productCarouselContainer}>
-      <Tabs>
-        {categories.map((category, i) => (
-          <Tab eventKey={category} title={category} key={i}>
-            <CarouselProvider
-              naturalSlideWidth={271}
-              naturalSlideHeight={271}
-              totalSlides={products.length}
-              visibleSlides={7}
-              className={styles.carousel}
-            >
-              <Slider className={styles.slider}>
-                {products.map((product, i) => (
-                  <Slide index={i} key={i} className={styles.slide}>
-                    <div className={styles.slideContainer}>
-                      {/*<Img className={styles.image} alt={product.node.title} fluid={product.node.heroImage.fluid} />*/}
-                      <img className={styles.image} alt={product.node.title} src={product.node.heroImage.fluid.src} />
-                      <div className={styles.title}>
-                        <Link to={`/equipment/${product.node.slug}`}>{product.node.title}</Link>
+class ProductCarousel extends Component {
+
+  render() {
+    console.log("categories", this.props.categories)
+    console.log("products", this.props.products)
+    console.log("this.props.windowWidth", this.props.windowWidth)
+
+    return (
+      <section className={`product-carousel ` + styles.productCarouselContainer}>
+        <Tabs>
+          {this.props.categories.map((category, i) => (
+            <Tab eventKey={category} title={category} key={i}>
+              <CarouselProvider
+                naturalSlideWidth={471}
+                naturalSlideHeight={471}
+                totalSlides={this.props.products.length}
+                visibleSlides={this.props.windowWidth > 991 ? 6 : this.props.windowWidth > 556 ? 3 : 2}
+                className={styles.carousel}
+              >
+                <Slider className={styles.slider}>
+                  {this.props.products.map((product, i) => (
+                    <Slide index={i} key={i} className={styles.slide}>
+                      <div className={styles.slideContainer}>
+                        <Link to={`/equipment/${product.node.slug}`}>
+                          <Img className={styles.image} alt={product.node.title} fluid={product.node.heroImage.fluid} />
+                          {/*<img className={styles.image} alt={product.node.title} src={product.node.heroImage.fluid.src} />*/}
+                          <div className={styles.title}>
+                            {product.node.title}
+                          </div>
+                        </Link>
                       </div>
-                    </div>
-                  </Slide>
-                ))}
-              </Slider>
-              <ButtonBack className={styles.backButton}>
-                <div className={styles.chevronArrowLeft}></div>
-              </ButtonBack>
-              <ButtonNext className={styles.nextButton}>
-                <div className={styles.chevronArrowRight}></div>
-              </ButtonNext>
-            </CarouselProvider>
-          </Tab>
-        ))}
-      </Tabs>
-    </section>
-)}
+                    </Slide>
+                  ))}
+                </Slider>
+                <ButtonBack className={styles.backButton}>
+                  <div className={styles.chevronArrowLeft}></div>
+                </ButtonBack>
+                <ButtonNext className={styles.nextButton}>
+                  <div className={styles.chevronArrowRight}></div>
+                </ButtonNext>
+              </CarouselProvider>
+            </Tab>
+          ))}
+        </Tabs>
+      </section>
+    )
+  }
+}
+
+export default windowSize(ProductCarousel);
