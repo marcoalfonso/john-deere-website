a3921ab3060ac3d005789159abd1d55a5563bf8c
Marco Lavielle <marcoalfonso@gmail.com>
2019-04-05 15:47:48 +1100

    fix/style-fixes

--- src/components/rich-text/rich-text.js
+++ src/components/rich-text/rich-text.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './rich-text.module.css'
 
 export default ({ body, richText }) => (
-  <div>
+  <div className={styles.richTextContainer}>
     { body && <div className={styles.richText} dangerouslySetInnerHTML={{__html: body}} /> }
     { richText && <div className={styles.richText} dangerouslySetInnerHTML={{__html: richText}} /> }
   </div>
